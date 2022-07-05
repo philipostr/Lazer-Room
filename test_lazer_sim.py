6c399504042e2a74c26cb99af400867dee6cad2e
Caroline Tippins <carotippins@hotmail.com>
2022-07-05 13:44:49 -0400

    Co-authored-by: Loïc Darboux <loicdrbx@users.noreply.github.com> Co-authored-by: philipostr <philipostr@users.noreply.github.com>

---
+++ test_lazer_sim.py
@@ -0,0 +1,32 @@
+import unittest
+from lazer_sim import *
+#from mutpy import commandline
+import sys
+
+class Test(unittest.TestCase):
+
+#TO TEST:
+# smallest_vec
+# generate_surroundings
+# distance
+# simulate
+
+    #Unit tests for smallest_vec method
+    def test_smallest_vec_pass(self):
+        self.assertEqual(smallest_vec([2,2]),(1,1))
+
+    @unittest.expectedFailure
+    def test_smallest_vec_fail(self):
+        self.assertEqual(smallest_vec([2,6]),(2,3))
+
+    #Unit tests for distance method
+    def test_distance_pass(self):
+        self.assertEqual(distance([5,6],[-7,11]),13)
+
+    @unittest.expectedFailure
+    def test_distance_fail(self):
+        self.assertEqual(distance([8,6]),9)
+
+if __name__ == '__main__':
+    unittest.main()
+    #commandline.main(sys.argv)
---
+++ test_tk_gui.py
@@ -0,0 +1,17 @@
+import unittest
+from tk_gui import *
+
+#TO TEST:
+# make_sims
+# create_sim
+
+class TestGUI(unittest.TestCase):
+
+    def test_make_sims(self):
+        pass
+
+    def test_create_sim(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
